6d27de9fefa558cb31cd0920a30dcd2347e36fcc
Никита Титов <ntitovgo@yandex.ru>
2025-09-18 16:48:02 +0300

    api постов

--- react-client/src/app/services/postsApi.ts
+++ react-client/src/app/services/postsApi.ts
@@ -1,5 +1,45 @@
-import { api } from "./api";
+import { Post } from "../types"
+import { api } from "./api"
 
 export const postApi = api.injectEndpoints({
-    endpoints: 
+  endpoints: builder => ({
+    createPost: builder.mutation<Post, { content: string }>({
+      query: postData => ({
+        url: "/posts",
+        method: "POST",
+        body: postData,
+      }),
+    }),
+    getAllPosts: builder.query<Post[], void>({
+      query: () => ({
+        url: "/posts",
+        method: "GET",
+      }),
+    }),
+    getPostById: builder.query<Post, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "GET",
+      }),
+    }),
+    deletePost: builder.mutation<Post, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
 })
+
+export const {
+  useCreatePostMutation,
+  useGetAllPostsQuery,
+  useGetPostByIdQuery,
+  useDeletePostMutation,
+  useLazyGetAllPostsQuery,
+  useLazyGetPostByIdQuery,
+} = postApi
+
+export const {
+  endpoints: { createPost, getAllPosts, getPostById, deletePost },
+} = postApi
