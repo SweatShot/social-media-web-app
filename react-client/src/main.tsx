8ec736b7cd351a06e830a25f28f156253f4f2a3b
Никита Титов <ntitovgo@yandex.ru>
2025-09-18 17:42:45 +0300

    темная тема

--- react-client/src/main.tsx
+++ react-client/src/main.tsx
@@ -5,8 +5,21 @@ import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
 import { HeroUIProvider } from "@heroui/react"
+import { createBrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "./components/theme-provider/index"
 
 const container = document.getElementById("root")
+
+const router = createBrowserRouter([
+  {
+    path: "/auth",
+    element: <h1>Auth</h1>,
+  },
+  {
+    path: "/",
+    element: <h1>Layout</h1>,
+  },
+])
 
 if (container) {
   const root = createRoot(container)
@@ -15,7 +28,9 @@ if (container) {
     <StrictMode>
       <Provider store={store}>
         <HeroUIProvider>
-          <App />
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
         </HeroUIProvider>
       </Provider>
     </StrictMode>,
