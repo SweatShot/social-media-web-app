9bd3a49be08067c15b42880a0cbeffd165a0ecf9
Никита Титов <ntitovgo@yandex.ru>
2025-09-21 16:43:06 +0300

    добавил отображение профиля

--- react-client/src/components/layout/index.tsx
+++ react-client/src/components/layout/index.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "../navbar"
 import { Outlet, useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { selectIsAuthenticated, selectUser } from "../../features/user/userSlice"
+import { Profile } from "../profile"
 
 export const Layout = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated)
@@ -27,8 +28,10 @@ export const Layout = () => {
         <div className="flex-1 p-4">
           <Outlet />
         </div>
-        <div>
-            ...
+        <div className="flex-2 p-4">
+          <div className="flex-col flex gap-5">
+            {!user && <Profile />}
+          </div>
         </div>
       </Container>
     </>
